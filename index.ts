180af3a96d7e64ef2692429925248375e169fa22
Wei <aszx70293@gmail.com>
2023-07-02 19:42:38 +0800

    20230702 - 1

--- index.ts
+++ index.ts
@@ -4,9 +4,20 @@ import express, {
   Response,
 } from 'express';
 import dotenv from 'dotenv';
+import { DataSource } from 'typeorm';
 
 const app: Express = express();
 dotenv.config();
+
+export const AppDataSource = new DataSource({
+  type: 'mysql',
+  host: 'localhost',
+  port: Number(process.env.MYSQL_LOCAL_PORT),
+  username: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DB,
+  synchronize: true,
+});
 
 const port = process.env.PORT;
 
@@ -14,4 +25,14 @@ app.get('/', (req: Request, res: Response) => {
   res.send('Express + TypeScript Server');
 });
 
-app.listen(port);
+AppDataSource.initialize()
+  .then(() => {
+    app.listen(port);
+    console.log('Data Source has been initialized!');
+  })
+  .catch((err) => {
+    console.error(
+      'Error during Data Source initialization',
+      err,
+    );
+  });
