e6e902d8423e97a5bcd35be56612b2757d7162da
Wei <aszx70293@gmail.com>
2023-07-09 17:18:41 +0800

    20230709 - 1

--- src/tasks/tasks.router.ts
+++ src/tasks/tasks.router.ts
@@ -1,5 +1,7 @@
 import { Request, Response, Router } from 'express';
+import { validationResult } from 'express-validator';
 import { TasksController } from './tasks.controller';
+import { createValidator } from './tasks.validator';
 
 export const tasksRouter: Router = Router();
 
@@ -9,5 +11,20 @@ tasksRouter.get(
     const taskController = new TasksController();
     const allTasks = await taskController.getAll();
     res.json(allTasks).status(200);
+  },
+);
+
+tasksRouter.post(
+  '/tasks',
+  createValidator,
+  //@ts-ignore
+  async (req: Request, res: Response) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res
+        .status(400)
+        .json({ errors: errors.array() });
+    }
   },
 );
